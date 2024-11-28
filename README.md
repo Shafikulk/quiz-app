73bf0bf5752b6787c04faadb3780ec251d594287
shafikulk <shafiqulislamcse45@gmail.com>
2024-11-28 21:28:46 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Categories Prompt
+
+i am trying to create data for ecommerce website. we have different categories and they include the following : televisions, fridges, microwaves, and laundry machines. i want to organise these categories in a json file as an array. for each category , it will have an id, title,description,image,keywords. write for me that array in the json format. do not include the array as akey in json file
---
+++ app/page.js
@@ -0,0 +1,70 @@
+"use client"
+import { useState } from 'react'
+import { questions } from '@/questions';
+
+export default function Home() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+  const question = questions[currentQuestion]
+
+  function handleOptionSelect(option) {
+    setSelectedOption(option);
+    setShowAnswer(true);
+    if (option === question.answer) {
+      setScore((prevScore) => prevScore + 1);
+    }
+  }
+
+  function handleNextClick() {
+    setCurrentQuestion((prevQn) => prevQn + 1);
+    setShowAnswer(false);
+    if (currentQuestion === questions.length - 1) {
+      setShowResults(true);
+      setCurrentQuestion(0);
+    }
+  }
+
+  function restartQuiz() {
+    setShowResults(false);
+    setCurrentQuestion(0);
+    setScore(0)
+  }
+  return(
+    <>
+    {showResults ? (
+        <div className="quiz-app">
+          <h2>Your Scores</h2>
+          <h3>
+            You scored {score} out {questions.length}
+          </h3>
+          <button onClick={restartQuiz}>Start the Quiz Again</button>
+        </div>
+      ):(
+      <div className='quiz-app'>
+        <div className='quiz-header'>
+            <h2>Awesome Quiz Application</h2>
+        </div>
+        <div className='quiz-body'>
+            <h1>{question.id} {question.question}</h1>
+            <div className='options'>
+                {
+                  question.options.map((option)=> {
+                    return(
+                      <button className={showAnswer&&option === question.answer?"correctAnswer":showAnswer&&option === selectedOption?"wrongAnswer":""} onClick={()=>handleOptionSelect(option)}>{option}</button>
+                    )
+                  })
+                }
+            </div>
+        </div>
+        <div className='quiz-footer'>
+            <p>{currentQuestion+1} out of {questions.length}</p>
+            <button onClick={handleNextClick} className="next">Next</button>
+        </div>
+    </div>
+    )}
+    </>
+  )
+}
